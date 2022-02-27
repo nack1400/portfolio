7b51431fdb01160089d3d41ae94dd8acff488e6c
nack1400 <nack1400@naver.com>
2022-02-27 23:23:36 +0900

    Add js: project filtering ani

--- main.js
+++ main.js
@@ -61,13 +61,18 @@ workBtnContainer.addEventListener("click", (e) => {
     return;
   }
 
-  projects.forEach((project) => {
-    if (filter === "*" || filter === project.dataset.type) {
-      project.classList.remove("invisible");
-    } else {
-      project.classList.add("invisible");
-    }
-  });
+  projectContainer.classList.add("anim-out");
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
 });
 
 function scrollIntoView(selector) {
