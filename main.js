0c3708e912937fe0fa4a786238a0633a6bfaef38
nack1400 <nack1400@naver.com>
2022-03-08 00:00:19 +0900

    Fix js: intersection observer

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-`use strict`;
+"use strict";
 
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
@@ -43,18 +42,18 @@ document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// Show "arrow up" button when scolling down
-const arrowUP = document.querySelector(".arrow-up");
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
-    arrowUP.classList.add("visible");
+    arrowUp.classList.add("visible");
   } else {
-    arrowUP.classList.remove("visible");
+    arrowUp.classList.remove("visible");
   }
 });
 
 // Handle click on the "arrow up" button
-arrowUP.addEventListener("click", () => {
+arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
 });
 
@@ -70,15 +69,15 @@ workBtnContainer.addEventListener("click", (e) => {
 
   // Remove selection from the previous item and select the new one
   const active = document.querySelector(".category__btn.selected");
-  active.classList.remove("selected");
-  const target =
-    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  if (active != null) {
+    active.classList.remove("selected");
+  }
   e.target.classList.add("selected");
 
   projectContainer.classList.add("anim-out");
-
   setTimeout(() => {
     projects.forEach((project) => {
+      console.log(project.dataset.type);
       if (filter === "*" || filter === project.dataset.type) {
         project.classList.remove("invisible");
       } else {
@@ -89,15 +88,9 @@ workBtnContainer.addEventListener("click", (e) => {
   }, 300);
 });
 
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
-}
-
-// 1. 모든 섹션 요소들을 가지고 온다
-// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
-// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
-
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
 const sectionIds = [
   "#home",
   "#about",
@@ -110,26 +103,31 @@ const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector(`[data-link="${id}"]`)
 );
-console.log(sections);
-console.log(navItems);
 
 let selectedNavIndex = 0;
 let selectedNavItem = navItems[0];
-function selectedNavItem(selected) {
+function selectNavItem(selected) {
   selectedNavItem.classList.remove("active");
   selectedNavItem = selected;
   selectedNavItem.classList.add("active");
 }
 
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
+
 const observerOptions = {
   root: null,
-  rootMargins: "0px",
+  rootMargin: "0px",
   threshold: 0.3,
 };
 
 const observerCallback = (entries, observer) => {
   entries.forEach((entry) => {
-    if (!entry.isIntersection && entry.IntersectionRatio > 0) {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      console.log("y");
       const index = sectionIds.indexOf(`#${entry.target.id}`);
       // 스크롤링이 아래로 되어서 페이지가 올라옴
       if (entry.boundingClientRect.y < 0) {
@@ -144,7 +142,7 @@ const observerCallback = (entries, observer) => {
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 sections.forEach((section) => observer.observe(section));
 
-window.addEventListener("scroll", () => {
+window.addEventListener("wheel", () => {
   if (window.scrollY === 0) {
     selectedNavIndex = 0;
   } else if (
@@ -153,5 +151,5 @@ window.addEventListener("scroll", () => {
   ) {
     selectedNavIndex = navItems.length - 1;
   }
-  selectedNavItem(navItems[selectedNavIndex]);
+  selectNavItem(navItems[selectedNavIndex]);
 });
